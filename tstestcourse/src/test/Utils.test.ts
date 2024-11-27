734479811fa7ada3656d760e0914b8723201053d
Nino Akut <akutnino@gmail.com>
2024-11-28 05:06:18 +0800

    Structure of an unit test

--- tstestcourse/src/test/Utils.test.ts
+++ tstestcourse/src/test/Utils.test.ts
@@ -1,8 +1,15 @@
 import { toUpperCase } from '../app/Utils.ts';
 
 describe('Utils test suite', () => {
-	test('should return uppercase', () => {
-		const result = toUpperCase('abc');
-		expect(result).toBe('ABC');
+	test('should return uppercase of valid string', () => {
+		// arrange:
+		const systemUnderTest = toUpperCase;
+		const expectedResult = 'ABC';
+
+		// act:
+		const actualResult = systemUnderTest('abc');
+
+		// asset:
+		expect(actualResult).toBe(expectedResult);
 	});
 });
