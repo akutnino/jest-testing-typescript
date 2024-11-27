9bd3a4e800eaed73becfba60d6f1396bcdb9aa4f
Nino Akut <akutnino@gmail.com>
2024-11-28 05:44:49 +0800

    Jest assertions and matchers

--- tstestcourse/src/app/Utils.ts
+++ tstestcourse/src/app/Utils.ts
@@ -1,5 +1,23 @@
+type stringInfo = {
+	lowerCase: string;
+	upperCase: string;
+	characters: string[];
+	length: number;
+	extraInfo: object | undefined;
+};
+
+function getStringInfo(props: string): stringInfo {
+	return {
+		lowerCase: props.toLowerCase(),
+		upperCase: props.toUpperCase(),
+		characters: Array.from(props),
+		length: props.length,
+		extraInfo: {},
+	};
+}
+
 function toUpperCase(props: string) {
 	return props.toUpperCase();
 }
 
-export { toUpperCase };
+export { toUpperCase, getStringInfo, stringInfo };
--- tstestcourse/src/test/Utils.test.ts
+++ tstestcourse/src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Utils.ts';
+import { getStringInfo, toUpperCase } from '../app/Utils.ts';
 
 describe('Utils test suite', () => {
 	test('should return uppercase of valid string', () => {
@@ -9,7 +9,37 @@ describe('Utils test suite', () => {
 		// act:
 		const actualResult = systemUnderTest('abc');
 
-		// asset:
+		// assert:
 		expect(actualResult).toBe(expectedResult);
+	});
+
+	test.only('should return info for valid string', () => {
+		// arrange:
+		const systemUnderTest = getStringInfo;
+		const expectedResult = 'my-string';
+
+		// act:
+		const actualResult = systemUnderTest('My-String');
+
+		// assert:
+		expect(actualResult.lowerCase).toBe(expectedResult); //use for primitive types / pass by value.
+		expect(actualResult.extraInfo).toEqual({}); //use for object types / pass by reference.
+
+		expect(actualResult.characters.length).toBe(9); // NOT CLEAN WAY.
+		expect(actualResult.characters).toHaveLength(9); // CLEAN WAY.
+
+		// prettier-ignore
+		expect(actualResult.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+
+		expect(actualResult.characters).toContain<string>('M');
+
+		expect(actualResult.characters).toEqual(
+			expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+		);
+
+		expect(actualResult.extraInfo).not.toBe(undefined);
+		expect(actualResult.extraInfo).not.toBeUndefined();
+		expect(actualResult.extraInfo).toBeDefined();
+		expect(actualResult.extraInfo).toBeTruthy();
 	});
 });
