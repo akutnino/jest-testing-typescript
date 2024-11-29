8a255c680b3dbea114955c85dc91977e64b56843
Nino Akut <akutnino@gmail.com>
2024-11-30 00:52:58 +0800

    clean multiple tests structure

--- tstestcourse/src/test/Utils.test.ts
+++ tstestcourse/src/test/Utils.test.ts
@@ -13,53 +13,39 @@ describe('Utils test suite', () => {
 		expect(actualResult).toBe(expectedResult);
 	});
 
-	describe('getStringInfo for arg My-String', () => {
-		test('should return string length', () => {
-			// arrage
-			const systemUnderTest = getStringInfo;
-			const expectedResult = 'My-string';
-
-			// act
-			const actualResult = systemUnderTest('My-String');
-
-			// assert
-			expect(actualResult).toHaveLength(expectedResult.length);
+	describe(`getStringInfo with agr: 'My-String'`, () => {
+		test('returns string length', () => {
+			const actualResult = getStringInfo('My-String');
+			expect(actualResult.characters).toHaveLength(9);
 		});
-		test('should return string in lower case', () => {
-			const systemUnderTest = getStringInfo;
-			const expectedResult = 'my-string';
-			const actualResult = systemUnderTest('My-String');
-			expect(actualResult.lowerCase).toBe(expectedResult);
+
+		test('returns string in lower case', () => {
+			const actualResult = getStringInfo('My-String');
+			expect(actualResult.lowerCase).toBe('my-string');
 		});
-	});
 
-	test.only('should return info for valid string', () => {
-		// arrange:
-		const systemUnderTest = getStringInfo;
-		const expectedResult = 'my-string';
+		test('returns string in upper case', () => {
+			const actualResult = getStringInfo('My-String');
+			expect(actualResult.upperCase).toBe('MY-STRING');
+		});
 
-		// act:
-		const actualResult = systemUnderTest('My-String');
+		test('returns correct string characters', () => {
+			const actualResult = getStringInfo('My-String');
+			// prettier-ignore
+			expect(actualResult.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+			expect(actualResult.characters).toContain<string>('M');
+			expect(actualResult.characters).toEqual(
+				expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+			);
+		});
 
-		// assert:
-		expect(actualResult.lowerCase).toBe(expectedResult); //use for primitive types / pass by value.
-		expect(actualResult.extraInfo).toEqual({}); //use for object types / pass by reference.
-
-		expect(actualResult.characters.length).toBe(9); // NOT CLEAN WAY.
-		expect(actualResult.characters).toHaveLength(9); // CLEAN WAY.
-
-		// prettier-ignore
-		expect(actualResult.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
-
-		expect(actualResult.characters).toContain<string>('M');
-
-		expect(actualResult.characters).toEqual(
-			expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
-		);
-
-		expect(actualResult.extraInfo).not.toBe(undefined);
-		expect(actualResult.extraInfo).not.toBeUndefined();
-		expect(actualResult.extraInfo).toBeDefined();
-		expect(actualResult.extraInfo).toBeTruthy();
+		test('returns defined extra info', () => {
+			const actualResult = getStringInfo('My-String');
+			expect(actualResult.extraInfo).toEqual({});
+			expect(actualResult.extraInfo).not.toBe(undefined);
+			expect(actualResult.extraInfo).not.toBeUndefined();
+			expect(actualResult.extraInfo).toBeDefined();
+			expect(actualResult.extraInfo).toBeTruthy();
+		});
 	});
 });
