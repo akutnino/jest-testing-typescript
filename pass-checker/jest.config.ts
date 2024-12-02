99bfd41ecf2cefe7520dc986454ded5fee3ceb3f
Nino Akut <akutnino@gmail.com>
2024-12-03 00:16:46 +0800

    added a testmatch dir to the jest config and added coverage folder to .gitignore

--- pass-checker/jest.config.ts
+++ pass-checker/jest.config.ts
@@ -1,10 +1,14 @@
 import { type Config } from '@jest/types';
 
+const baseDir = '<rootDir>/src/app';
+const baseTestDir = '<rootDir>/src/test';
+
 const config: Config.InitialOptions = {
 	preset: 'ts-jest',
 	verbose: true,
 	collectCoverage: true,
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: [`${baseDir}/**/*.ts`],
+	testMatch: [`${baseTestDir}/**/*.ts`],
 	testEnvironment: 'node',
 	transform: {
 		'^.+.tsx?$': ['ts-jest', {}],
