dffc724838708f7c2919d5e7c3bbbf7954c13b79
Nino Akut <akutnino@gmail.com>
2024-12-05 10:21:04 +0800

    PassChecker Iteration -recode

--- pass-checker/src/app/PasswordChecker.ts
+++ pass-checker/src/app/PasswordChecker.ts
@@ -1,3 +1,42 @@
 export class PasswordChecker {
-	public checkPassword() {}
+	public checkPassword(password: string): boolean {
+		const noUpperCase = [...password]
+			.map((char) => {
+				const charIsNumber = isFinite(Number(char));
+				if (charIsNumber) return false;
+
+				const charIsAlphabet =
+					(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) ||
+					(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90);
+				if (!charIsAlphabet) return false;
+
+				const charToUpperCase = String.fromCharCode(char.charCodeAt(0)).toUpperCase();
+				const charIsUpperCase = charToUpperCase === char;
+				if (charIsUpperCase) return true;
+				return false;
+			})
+			.every((bool) => bool === false);
+
+		const noLowerCase = [...password]
+			.map((char) => {
+				const charIsNumber = isFinite(Number(char));
+				if (charIsNumber) return false;
+
+				const charIsAlphabet =
+					(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) ||
+					(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90);
+				if (!charIsAlphabet) return false;
+
+				const charToLowerCase = String.fromCharCode(char.charCodeAt(0)).toLowerCase();
+				const charIsLowerCase = charToLowerCase === char;
+				if (charIsLowerCase) return true;
+				return false;
+			})
+			.every((bool) => bool === false);
+
+		if (password.length < 8) return false;
+		if (noUpperCase) return false;
+		if (noLowerCase) return false;
+		return true;
+	}
 }
--- pass-checker/src/test/PasswordChecker.test.ts
+++ pass-checker/src/test/PasswordChecker.test.ts
@@ -7,7 +7,33 @@ describe('PasswordChecker test suite', () => {
 		systemUnderTest = new PasswordChecker();
 	});
 
-	test('should do nothing for the moment', () => {
-		systemUnderTest.checkPassword();
+	test('password with less than 8 chars is invalid', () => {
+		const actualResult = systemUnderTest.checkPassword('1234567');
+		expect(actualResult).toBe(false);
+	});
+
+	test('password with more than 8 chars is valid', () => {
+		const actualResult = systemUnderTest.checkPassword('12345678');
+		expect(actualResult).toBe(true);
+	});
+
+	test('password with no upper case char is invalid', () => {
+		const actualResult = systemUnderTest.checkPassword('1234abcd');
+		expect(actualResult).toBe(false);
+	});
+
+	test('password with a upper case char is valid', () => {
+		const actualResult = systemUnderTest.checkPassword('1234Abcd');
+		expect(actualResult).toBe(true);
+	});
+
+	test('password with no lower case char is invalid', () => {
+		const actualResult = systemUnderTest.checkPassword('1234ABCD');
+		expect(actualResult).toBe(false);
+	});
+
+	test('password with a lower case char is valid', () => {
+		const actualResult = systemUnderTest.checkPassword('1234ABCd');
+		expect(actualResult).toBe(true);
 	});
 });
