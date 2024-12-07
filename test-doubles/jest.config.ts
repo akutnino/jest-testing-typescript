3a647d69de84ad3fa86a3f78df4dbd43e883f825
Nino Akut <akutnino@gmail.com>
2024-12-08 02:17:00 +0800

    first test-doubles project commit

---
+++ test-doubles/jest.config.ts
@@ -0,0 +1,18 @@
+import { type Config } from '@jest/types';
+
+const baseDir = '<rootDir>/src/app';
+const testDir = '<rootDir>/src/test';
+
+const config: Config.InitialOptions = {
+	preset: 'ts-jest',
+	verbose: true,
+	collectCoverage: true,
+	collectCoverageFrom: [`${baseDir}/**/*.ts`],
+	testMatch: [`testDir/**/*.ts`],
+	testEnvironment: 'node',
+	transform: {
+		'^.+.tsx?$': ['ts-jest', {}],
+	},
+};
+
+export default config;
