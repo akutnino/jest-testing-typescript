731634c61b96d9ad0425cf477e4daefb99370f4c
Nino Akut <akutnino@gmail.com>
2024-12-10 03:48:41 +0800

    intro to mocks doubles in jest

--- test-doubles/src/test/OtherUtils.test.ts
+++ test-doubles/src/test/OtherUtils.test.ts
@@ -2,7 +2,6 @@ import { calculateComplexity, toUpperCaseWithCB } from '../app/OtherUtils.ts';
 
 // DUMMY OBJECTS: passed around but not used
 // SPIES: tracks information about how a unit is called
-// MOCKS: preprogrammed with expectation
 
 describe('OtherUtils test suite', () => {
 	test('should calculate complexity', () => {
@@ -30,5 +29,60 @@ describe('OtherUtils test suite', () => {
 		const fakeFunction = () => {};
 		const actualResult = toUpperCaseWithCB('abc', fakeFunction);
 		expect(actualResult).toBe('ABC');
+	});
+
+	describe('Tracking callbacks WITHOUT Jest mocks', () => {
+		// WRITE OUR OWN MOCKS:
+		// MOCKS: preprogrammed with expectation
+
+		let cbArgs: string[] = [];
+		let timesCalled = 0;
+
+		function callbackMock(arg: string) {
+			cbArgs.push(arg);
+			timesCalled += 1;
+		}
+
+		afterEach(() => {
+			// clearing tracking fields.
+			cbArgs = [];
+			timesCalled = 0;
+		});
+
+		test('ToUpperCase - calls callback for invalid argument', () => {
+			const actualResult = toUpperCaseWithCB('', callbackMock);
+			expect(actualResult).toBeUndefined();
+			expect(cbArgs).toContain('Invalid Argument');
+			expect(timesCalled).toBe(1);
+		});
+
+		test('ToUpperCase - calls callback for valid argument', () => {
+			const actualResult = toUpperCaseWithCB('ABC', callbackMock);
+			expect(actualResult).toBe('ABC');
+			expect(cbArgs).toContain('Called function with arg/s: ABC');
+			expect(timesCalled).toBe(1);
+		});
+	});
+
+	describe('Tracking callbacks WITH Jest mocks', () => {
+		const callbackMock = jest.fn();
+
+		afterEach(() => {
+			jest.clearAllMocks();
+		});
+
+		test('ToUpperCase - calls callback for invalid argument', () => {
+			const actualResult = toUpperCaseWithCB('', callbackMock);
+			expect(actualResult).toBeUndefined();
+			expect(callbackMock).toHaveBeenCalledWith('Invalid Argument');
+			expect(callbackMock).toHaveBeenCalledTimes(1);
+		});
+
+		test('ToUpperCase - calls callback for valid argument', () => {
+			const actualResult = toUpperCaseWithCB('ABC', callbackMock);
+			expect(actualResult).toBe('ABC');
+			expect(callbackMock).toHaveBeenCalledWith('Called function with arg/s: ABC');
+			expect(callbackMock).toHaveBeenCalledTimes(1);
+		});
 	});
 });
