b88760cc569b308d3075d43c6dbbf24433cb7233
Nino Akut <akutnino@gmail.com>
2024-12-15 01:16:10 +0800

    created new types for the database generic

--- server-app/src/test/DataBase.test.ts
+++ server-app/src/test/DataBase.test.ts
@@ -1,3 +1,13 @@
+import { DataBase } from '../app/data/DataBase.ts';
+
+type someTypeWithId = {
+	id: string;
+	name: string;
+	color: string;
+};
+
 describe('Database test suite', () => {
+	let systemUnderTest: DataBase<someTypeWithId>;
+
 	test('', () => {});
 });
